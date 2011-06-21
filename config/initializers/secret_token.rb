e115bae20290cfd9b2a4a9309236a806a73ce306
Maciej Litwiniuk <maciej@galdomedia.pl>
2011-06-21 09:48:45 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubbit::Application.config.secret_token = '86312ab5112c071400b20ce4999e291f313e50dac0bcf70cdcae9bd571913275919409275c50a24c50d6297b73b9a40b25a810eddee14bf40119fa112a16dbd6'
